b4af330be0d8fcf9088b0ec5985dbe040658d755
rainstar <rainstar.general@googlemail.com>
2014-05-11 15:05:38 +0100

    Plot1 and 2 minor updates + plot3 and plot 4 added

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 ## Household Power Consumption Peer Assignment, Plot 4
 ## ./electric/household_power_consumption.txt
-## Anahita Saghafi 10/05/2014
+## Anahita Saghafi 11/05/2014
 ##--------------------------------------------------------------------        
 
 
@@ -44,14 +44,28 @@ DF$DateTime <- as.POSIXct(strptime(paste(DF$Date,DF$Time), "%d/%m/%Y %H:%M:%S"))
 library(datasets)
 
 x <- DF$DateTime
-y<- as.numeric(as.character(DF$Global_active_power))
-
-##DF$Day <- weekdays(as.Date(DF$Date))
+a<- as.numeric(as.character(DF$Global_active_power))
+b<- as.numeric(as.character(DF$Voltage))
+c<- as.numeric(as.character(DF$Global_reactive_power))
+y1<- DF$Sub_metering_1
+y2<- DF$Sub_metering_2
+y3<- DF$Sub_metering_3
 
 
 png("plot4.png", width = 480, height = 480)
-with(DF, plot(x,y, type="l", xlab="", ylab="Global Active Power(Kilowatts)" ))
+par(mfrow=c(2,2))
+with(DF,{
+        plot(x,a, type="l", xlab="", ylab="Global Active Power(Kilowatts)" )
+        plot(x,b, type="l", xlab="datetime", ylab="Voltage)" )
+        
+        with(DF, plot(x,y1, type="l",ylab = "Energy sub metering", xlab=""))
+        with(subset(DF, colname = "Sub_metering_2"), lines(x,y2, type="l", col="red"))
+        with(subset(DF, colname = "Sub_metering_3"), lines(x,y3, type="l", col="blue"))
+        
+        legend("topright",cex=0.7, lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(x,c, type="l", xlab="datetime",ylab="global_rective_power )
+})
 dev.off()
 
 
-## Complete!   
+## Complete!     
